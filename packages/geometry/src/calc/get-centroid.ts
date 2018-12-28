a396f6543f09c38a608f74f6ba4ab3f59b9efe8d
alberto <alberto.massa@ymail.com>
2018-12-28 12:27:06 +0100

    triangle centroid tailored for 3d points

--- packages/geometry/src/calc/get-centroid.ts
+++ packages/geometry/src/calc/get-centroid.ts
@@ -1,4 +1,5 @@
 import { Vec3 } from '@tstackgl/types'
+import vec3 from 'gl-vec3'
 
 export function getCentroid<T extends Array<number> | Float32Array>(points: Array<T>): T {
   // TODO: refactor with transducers
@@ -28,4 +29,14 @@ export function getCentroid<T extends Array<number> | Float32Array>(points: Arra
 
 export function getCentroidFromCells(cells: Array<Vec3>, positions: Array<Vec3>): Array<Vec3> {
   return cells.map(face => getCentroid(face.map(index => positions[index])))
+}
+
+export function getCentroidTriangle3(triangle: [Vec3, Vec3, Vec3]) {
+  const result: Vec3 = vec3.create()
+
+  result[0] = (triangle[0][0] + triangle[1][0] + triangle[2][0]) / 3
+  result[1] = (triangle[0][1] + triangle[1][1] + triangle[2][1]) / 3
+  result[2] = (triangle[0][2] + triangle[1][2] + triangle[2][2]) / 3
+
+  return result
 }
