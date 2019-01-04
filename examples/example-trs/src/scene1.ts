b1177f9c55bab36332c25fea9734400b4b2f1876
alberto <alberto.massa@ymail.com>
2019-01-04 17:25:24 +0100

    example-trs: draw all cylinder

--- examples/example-trs/src/scene1.ts
+++ examples/example-trs/src/scene1.ts
@@ -20,12 +20,64 @@ import quat from 'gl-quat'
 import { createDrawMeshNormalLine } from './draw-normal-line'
 import { Quat } from '@tstackgl/types'
 import { polygonToCells } from './polygonToCells'
-import { angleBetweenSegments } from '@tstackgl/geometry'
 import { createIcosahedron } from './primitive-icosahedron'
 
+const cylinderHeight = 100
 let loop = { cancel: function() {} }
 
-const cylinderHeight = 100
+type Triangle3 = [Vec3, Vec3, Vec3]
+
+const angles = [
+  Math.PI / 2,
+  0.52,
+  0.68,
+  0.36,
+  -0.36,
+  -0.68,
+  -0.52,
+  0.52,
+  0,
+  -Math.PI / 3,
+  -Math.PI / 3,
+  0,
+  -0.65,
+  -0.39,
+  0.66,
+  0.38,
+  0.91,
+  0.13,
+  -0.92,
+  -0.13,
+]
+
+function computeMatrixToAlignCylinder(triangle: Triangle3, index: number) {
+  const center = getCentroidTriangle3(triangle)
+  const dir = vec3.normalize(vec3.create(), center)
+
+  const triangleFacingYMatrix = mat4.fromQuat(
+    mat4.create(),
+    quat.rotationTo(quat.create(), dir, [0, 1, 0]),
+  )
+
+  // const triangleFa`cingY2 = triangle.map(([x, y, z]) => [x, z] as Vec2)
+  // const p1 = triangleFacingY2[1]
+  // const p2 = triangleFacingY2[2]
+  // const a = Math.abs(Math.atan2(p2[1] - p1[1], p2[0] - p1[0]))
+  const angleTriangle = angles[index]
+  const delta = vec3.dist([0, 0, 0], center)
+  const mat = mat4.create()
+  const q: Quat = quat.rotationTo(quat.create(), [0, 1, 0], dir)
+  const rotationMat = mat4.fromQuat(mat4.create(), q)
+  const translate = mat4.fromTranslation(mat4.create(), [0, cylinderHeight / 2 + delta, 0])
+  const scale = mat4.fromScaling(mat4.create(), [3, 1, 3])
+  const rotation = mat4.fromRotation(mat4.create(), angleTriangle, [0, 1, 0])
+  mat4.multiply(mat, mat, rotationMat)
+  mat4.multiply(mat, mat, translate)
+  mat4.multiply(mat, mat, scale)
+  mat4.multiply(mat, mat, rotation)
+
+  return { mat, dir }
+}
 
 export function createReglScene1() {
   const init = (canvas: HTMLCanvasElement, __: WebGLRenderingContext) => {
@@ -39,7 +91,7 @@ export function createReglScene1() {
 
     const regl = createRegl({ canvas, extensions: ['OES_standard_derivatives'] })
     const frameCatch = createFrameCatch(regl)
-    const camera = createCamera(regl, { distance: 400, phi: 100 + 0.4, theta: 0.4 })
+    const camera = createCamera(regl, { distance: 600, phi: 100 + 0.4, theta: 0.4 })
     const axis = createXYZ(regl, 150)
 
     /* 
@@ -47,75 +99,33 @@ export function createReglScene1() {
     */
 
     const icosahedron: Mesh = createIcosahedron(100) //createIcosphere(100, { subdivisions: 1 })
-    const cylinder: Mesh = createCylinder(20, 20, cylinderHeight, 3)
+    const cylinder: Mesh = createCylinder(10, 20, cylinderHeight, 3)
 
     console.log({ icosahedron })
 
-    type Triangle3 = [Vec3, Vec3, Vec3]
-    const triangle = icosahedron.cells[11].map(index => icosahedron.positions[index]) as Triangle3
-    const center = getCentroidTriangle3(triangle)
-
-    const angles = [
-      0,
-      0.52,
-      0.68,
-      0.36,
-      -0.36,
-      -0.68,
-      -0.52,
-      0.52,
-      0,
-      -Math.PI / 3,
-      -Math.PI / 3,
-      0,
-    ]
-
     /* 
-      ------------------------------------------ calculate model matrix
+    ------------------------------------------ calculate model matrix
     */
 
-    const dir = vec3.normalize(vec3.create(), center)
-
-    const triangleFacingYMatrix = mat4.fromQuat(
-      mat4.create(),
-      quat.rotationTo(quat.create(), dir, [0, 1, 0]),
+    const xform = tx.comp(
+      tx.map((cell: Vec3) => cell.map(index => icosahedron.positions[index]) as Triangle3),
+      tx.mapIndexed((i: number, triangle: Triangle3) => computeMatrixToAlignCylinder(triangle, i)),
+      tx.map(({ mat, dir }) => ({
+        model: mat,
+        diffuseColor: vec3.normalize(vec3.create(), dir),
+        ambientColor: [0.1, 0.1, 0.1] as Vec3,
+        lightDirection: [100, 0, 0] as Vec3,
+      })),
     )
-
-    const triangleFacingY = triangle.map(point =>
-      vec3.transformMat4(vec3.create(), point, triangleFacingYMatrix),
-    )
-    const triangleFacingY2 = triangle.map(([x, y, z]) => [x, z] as Vec2)
-    const p1 = triangleFacingY2[1]
-    const p2 = triangleFacingY2[2]
-    const a = Math.abs(Math.atan2(p2[1] - p1[1], p2[0] - p1[0]))
-    const angleTriangle = 0
-    const delta = vec3.dist([0, 0, 0], center)
-    const mat = mat4.create()
-    const q: Quat = quat.rotationTo(quat.create(), [0, 1, 0], dir)
-    const rotationMat = mat4.fromQuat(mat4.create(), q)
-    const translate = mat4.fromTranslation(mat4.create(), [0, cylinderHeight / 2 + delta, 0])
-    const scale = mat4.fromScaling(mat4.create(), [3, 1, 3])
-    const rotation = mat4.fromRotation(mat4.create(), angleTriangle, [0, 1, 0])
-    mat4.multiply(mat, mat, rotationMat)
-    mat4.multiply(mat, mat, translate)
-    mat4.multiply(mat, mat, scale)
-    mat4.multiply(mat, mat, rotation)
+    const props = tx.transduce(xform, tx.push(), icosahedron.cells)
 
     /* 
       ------------------------------------------ create draw commands
     */
 
-    const drawNormal = createDrawMeshNormalLine(regl, center, dir, 50)
-    const props: PropsBasicMaterial = {
-      model: mat,
-      diffuseColor: [1, 0.2, 0.2],
-      ambientColor: [0.1, 0.1, 0.1],
-      lightDirection: [100, 0, 0],
-    }
     const drawIcosphere = createDrawMeshWireframe(regl, icosahedron)
     const drawCylinder = createBasicMesh(regl, cylinder)
     const drawDebug = createDrawPointDebug(regl, 5)
-    const drawTriangleFacingY = createDrawMeshUnicolor(regl, polygonToCells(triangleFacingY))
 
     /* 
       ------------------------------------------ draw loop
