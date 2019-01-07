694b014febfc685c47bf6a2b0ef32bf724bbca6a
alberto <alberto.massa@ymail.com>
2019-01-07 16:32:27 +0100

    standford dragon typings

---
+++ packages/stackgl-types/src/stanford-dragon.d.ts
@@ -0,0 +1,56 @@
+// https://github.com/hughsk/stanford-dragon
+
+declare module 'stanford-dragon' {
+  import { Vec3 } from '@tstackgl/types'
+
+  const b: {
+    positions: Array<Vec3>
+    cells: Array<Vec3>
+  }
+
+  export = b
+}
+
+declare module 'stanford-dragon/1' {
+  import { Vec3 } from '@tstackgl/types'
+
+  const b: {
+    positions: Array<Vec3>
+    cells: Array<Vec3>
+  }
+
+  export = b
+}
+
+declare module 'stanford-dragon/2' {
+  import { Vec3 } from '@tstackgl/types'
+
+  const b: {
+    positions: Array<Vec3>
+    cells: Array<Vec3>
+  }
+
+  export = b
+}
+
+declare module 'stanford-dragon/3' {
+  import { Vec3 } from '@tstackgl/types'
+
+  const b: {
+    positions: Array<Vec3>
+    cells: Array<Vec3>
+  }
+
+  export = b
+}
+
+declare module 'stanford-dragon/4' {
+  import { Vec3 } from '@tstackgl/types'
+
+  const b: {
+    positions: Array<Vec3>
+    cells: Array<Vec3>
+  }
+
+  export = b
+}
