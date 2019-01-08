55910a888bb6ae8e8769e9d0e6056b755fd078b3
alberto <alberto.massa@ymail.com>
2019-01-09 00:57:26 +0100

    commons.d.ts handles .vert and .frag as string

--- packages/stackgl-types/src/commons.d.ts
+++ packages/stackgl-types/src/commons.d.ts
@@ -5,3 +5,13 @@ declare module '*.png' {}
 declare module '*.svg' {}
 
 declare module '*.css' {}
+
+declare module '*.vert' {
+  const content: string
+  export default content
+}
+
+declare module '*.frag' {
+  const content: string
+  export default content
+}
